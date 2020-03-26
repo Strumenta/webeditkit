d2d97e64b3f986b1c61f3934fc1d71d7a7779453
Federico Tomassetti <federico@tomassetti.me>
2020-03-26 17:50:25 +0100

    refactoring webeditkit

--- src/webeditkit.ts
+++ src/webeditkit.ts
@@ -1,20 +1,20 @@
-//import renderers from "./renderer";
-
 import {renderModelNode} from "./renderer";
 
 import {dataToNode, forEachDataModel, setDatamodelRoot} from "./datamodel";
 
-var snabbdom = require('snabbdom/snabbdom');
-var patch = snabbdom.init([ // Init patch function with chosen modules
+import {init} from "snabbdom/snabbdom";
+
+import h from "snabbdom/h"; // helper function for creating vnodes
+
+import toVNode from "snabbdom/tovnode";
+
+const patch = init([ // Init patch function with chosen modules
     require('snabbdom/modules/class').default, // makes it easy to toggle classes
     require('snabbdom/modules/props').default, // for setting properties on DOM elements
     require('snabbdom/modules/style').default, // handles styling on elements with support for animations
     require('snabbdom/modules/eventlisteners').default, // attaches event listeners
     require('snabbdom/modules/dataset').default,
 ]);
-var h = require('snabbdom/h').default; // helper function for creating vnodes
-const toVNode = require('snabbdom/tovnode').default;
-
 let vnodes = {};
 
 let renderDataModels = function() {
