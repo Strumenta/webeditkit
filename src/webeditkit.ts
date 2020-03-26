5bdef58b400285a74a9366e2ed3f232b57717172
Federico Tomassetti <federico@tomassetti.me>
2020-03-26 17:54:36 +0100

    refactoring webeditkit

--- src/webeditkit.ts
+++ src/webeditkit.ts
@@ -8,19 +8,25 @@ import h from "snabbdom/h"; // helper function for creating vnodes
 
 import toVNode from "snabbdom/tovnode";
 
-const patch = init([ // Init patch function with chosen modules
-    require('snabbdom/modules/class').default, // makes it easy to toggle classes
-    require('snabbdom/modules/props').default, // for setting properties on DOM elements
-    require('snabbdom/modules/style').default, // handles styling on elements with support for animations
-    require('snabbdom/modules/eventlisteners').default, // attaches event listeners
-    require('snabbdom/modules/dataset').default,
-]);
-let vnodes = {};
+import * as sclass from "snabbdom/modules/class";
+import * as sprops from "snabbdom/modules/props";
+import * as sstyle from "snabbdom/modules/style";
+import * as seventlisteners from "snabbdom/modules/eventlisteners";
+import * as sdataset from "snabbdom/modules/dataset";
 
-let renderDataModels = function() {
-    forEachDataModel(function(name, root) {
-        let vnode = h('div#' + name + '.editor', {}, [renderModelNode(root)]);
-        if (vnodes[name] == undefined) {
+const patch = init([ // Init patch function with chosen modules
+    sclass.default, // makes it easy to toggle classes
+    sprops.default, // for setting properties on DOM elements
+    sstyle.default, // handles styling on elements with support for animations
+    seventlisteners.default, // attaches event listeners
+    sdataset.default,
+]);
+const vnodes = {};
+
+const renderDataModels = () => {
+    forEachDataModel((name, root) => {
+        const vnode = h('div#' + name + '.editor', {}, [renderModelNode(root)]);
+        if (vnodes[name] === undefined) {
             vnodes[name] = toVNode($("div#"+name)[0]);
         }
         vnodes[name] = patch(vnodes[name], vnode);
