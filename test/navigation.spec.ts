6af5cc785bbdbb2ac4de42e4786442716028f822
Federico Tomassetti <federico@tomassetti.me>
2020-03-29 15:34:19 +0200

    testing move to next element

---
+++ test/navigation.spec.ts
@@ -0,0 +1,66 @@
+import {registerDataModelClass, dataToNode, ModelNode, NodeData, Ref} from '../src/datamodel';
+import { expect } from 'chai';
+import 'mocha';
+import {moveToNextElement} from "../src/navigation";
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+describe('Navigation', () => {
+
+    it('should support moveFocusToStart', () => {
+        const dom = new JSDOM(`<html>
+\t<body data-gr-c-s-loaded="true">
+\t\t<div id="calc" class="editor">
+\t\t\t<div class="vertical-group represent-node" data-node_represented="324292001770075100">
+\t\t\t\t<div class="row">
+\t\t\t\t\t<input class="fixed title" style="width: 120.875px;">
+\t\t\t\t\t<input class="editable title" placeholder="<no name>" required="" style="width: 151.688px;">
+\t\t\t\t</div>
+\t\t\t\t<div class="row"></div>
+\t\t\t\t<div class="row">
+\t\t\t\t\t<input class="fixed strong" style="width: 39.8375px;">
+\t\t\t\t</div>
+\t\t\t\t<div class="row">
+\t\t\t\t\t<div class="tab"></div>
+\t\t\t\t\t<div class="vertical-collection represent-collection"data-relation_represented="inputs">
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547698">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547702" style="width: 45.2875px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685575196">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 26.6px;">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685575206" style="width: 45.2875px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547705">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547711" style="width: 34.0125px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t</div>
+\t</body>
+</html>`)
+        const doc = dom.window.document;
+        // @ts-ignore
+        global.$ = require('jquery')(dom.window);
+
+        const editableName_a = doc.querySelector('div[data-node_represented="1848360241685547698"] .editable');
+        const keyword_a = doc.querySelector('div[data-node_represented="1848360241685547698"] .keyword');
+        editableName_a.focus();
+        expect(doc.activeElement).to.equals(editableName_a);
+        moveToNextElement(editableName_a);
+        expect(doc.activeElement).to.equals(keyword_a);
+    });
+
+});
