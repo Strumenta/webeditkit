e776376a39dc1c0d4f0fb1ff1d4569a0a751e389
Federico Tomassetti <federico@tomassetti.me>
2020-04-01 13:44:45 +0200

    adding tests for cells support

---
+++ test/cells_support.spec.ts
@@ -0,0 +1,165 @@
+import {dataToNode, ModelNode} from '../src/datamodel';
+import { expect } from 'chai';
+import 'mocha';
+import {clearRendererRegistry, getRegisteredRenderer, renderModelNode} from "../src/renderer";
+import {VNode} from "snabbdom/vnode";
+import {h} from "snabbdom";
+import {registerRenderer} from "../src/renderer";
+import {fixedCell, referenceCell, row} from "../src/cells";
+import {addId, flattenArray, setDataset} from "../src/cells/support";
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+var init = require('snabbdom-to-html/init')
+var modules = require('snabbdom-to-html/modules/index')
+var toHTML = init([
+    modules.class,
+    modules.props,
+    modules.attributes,
+    modules.style,
+    modules.dataset
+])
+
+const html1 = `<html>
+\t<body data-gr-c-s-loaded="true">
+\t\t<div id="calc" class="editor">
+\t\t\t<div class="vertical-group represent-node" data-node_represented="324292001770075100">
+\t\t\t\t<div class="row">
+\t\t\t\t\t<input class="fixed title" style="width: 120.875px;">
+\t\t\t\t\t<input class="editable title" placeholder="<no name>" required="" style="width: 151.688px;">
+\t\t\t\t</div>
+\t\t\t\t<div class="row"></div>
+\t\t\t\t<div class="row">
+\t\t\t\t\t<input class="fixed strong" style="width: 39.8375px;">
+\t\t\t\t</div>
+\t\t\t\t<div class="row">
+\t\t\t\t\t<div class="tab"></div>
+\t\t\t\t\t<div class="vertical-collection represent-collection"data-relation_represented="inputs">
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547698">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547702" style="width: 45.2875px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685575196">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 26.6px;" value="sdsd">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685575206" style="width: 45.2875px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t\t<div class="row">
+\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547705">
+\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
+\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
+\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547711" style="width: 34.0125px;">
+\t\t\t\t\t\t\t</div>
+\t\t\t\t\t\t</div>
+\t\t\t\t\t</div>
+\t\t\t\t</div>
+\t\t\t</div>
+\t\t</div>
+\t</body>
+</html>`;
+
+function compareVNodes(rendered: VNode, expectedRendered: VNode) : void {
+    expect(rendered.sel).to.eql(expectedRendered.sel);
+    expect(rendered.data.props).to.eql(expectedRendered.data.props);
+    expect(rendered.data.dataset).to.eql(expectedRendered.data.dataset);
+    expect(rendered.children).deep.equal(expectedRendered.children);
+    expect(rendered.key).to.eql(expectedRendered.key);
+    expect(rendered.text).to.eql(expectedRendered.text);
+}
+
+const rootData1 = {
+    "children": [
+        {
+            "containingLink": "inputs",
+            "children": [
+                {
+                    "containingLink": "type",
+                    "children": [],
+                    "properties": {},
+                    "refs": {},
+                    "id": {
+                        "regularId": "1848360241685547702"
+                    },
+                    "concept": "com.strumenta.financialcalc.BooleanType",
+                    "abstractConcept": false
+                }
+            ],
+            "properties": {
+                "name": "a"
+            },
+            "refs": {},
+            "id": {
+                "regularId": "1848360241685547698"
+            },
+            "name": "a",
+            "concept": "com.strumenta.financialcalc.Input",
+            "abstractConcept": false
+        },
+        {
+            "containingLink": "inputs",
+            "children": [
+                {
+                    "containingLink": "type",
+                    "children": [],
+                    "properties": {},
+                    "refs": {},
+                    "id": {
+                        "regularId": "1848360241685547711"
+                    },
+                    "concept": "com.strumenta.financialcalc.StringType",
+                    "abstractConcept": false
+                }
+            ],
+            "properties": {
+                "name": "b"
+            },
+            "refs": {},
+            "id": {
+                "regularId": "1848360241685547705"
+            },
+            "name": "b",
+            "concept": "com.strumenta.financialcalc.Input",
+            "abstractConcept": false
+        }
+    ],
+    "properties": {
+        "name": "My calculations"
+    },
+    "refs": {},
+    "id": {
+        "regularId": "324292001770075100"
+    },
+    "name": "My calculations",
+    "concept": "com.strumenta.financialcalc.FinancialCalcSheet",
+    "abstractConcept": false
+};
+
+describe('Cells.Support', () => {
+
+    describe('should support setDataset', () => {
+        it('it should be rendered in a certain way', () => {
+            const aNode = dataToNode(rootData1);
+            let cell = fixedCell(aNode, 'My fixed test');
+            expect(toHTML(cell)).to.eql('<input class="fixed" value="My fixed test">');
+            cell = setDataset(cell, {a: 123});
+            expect(toHTML(cell)).to.eql('<input class="fixed" value="My fixed test" data-a="123">');
+        })
+    });
+
+    describe('should support addId', () => {
+        it('it should be rendered in a certain way', () => {
+            const aNode = dataToNode(rootData1);
+            let cell = fixedCell(aNode, 'My fixed test');
+            expect(toHTML(cell)).to.eql('<input class="fixed" value="My fixed test">');
+            cell = addId(cell, 'spritz');
+            expect(toHTML(cell)).to.eql('<input id="spritz" class="fixed" value="My fixed test">');
+        })
+    });
+
+});
