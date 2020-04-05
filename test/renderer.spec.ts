5e35e57ce36837aa8794e632eb97a35488e36471
Federico Tomassetti <federico@tomassetti.me>
2020-04-05 08:52:23 +0200

    refactor renderer tests

--- test/renderer.spec.ts
+++ test/renderer.spec.ts
@@ -5,61 +5,7 @@ import {clearRendererRegistry, getRegisteredRenderer, renderModelNode} from "../
 import {VNode} from "snabbdom/vnode";
 import {h} from "snabbdom";
 import {registerRenderer} from "../src/renderer";
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-const html1 = `<html>
-\t<body data-gr-c-s-loaded="true">
-\t\t<div id="calc" class="editor">
-\t\t\t<div class="vertical-group represent-node" data-node_represented="324292001770075100">
-\t\t\t\t<div class="row">
-\t\t\t\t\t<input class="fixed title" style="width: 120.875px;">
-\t\t\t\t\t<input class="editable title" placeholder="<no name>" required="" style="width: 151.688px;">
-\t\t\t\t</div>
-\t\t\t\t<div class="row"></div>
-\t\t\t\t<div class="row">
-\t\t\t\t\t<input class="fixed strong" style="width: 39.8375px;">
-\t\t\t\t</div>
-\t\t\t\t<div class="row">
-\t\t\t\t\t<div class="tab"></div>
-\t\t\t\t\t<div class="vertical-collection represent-collection"data-relation_represented="inputs">
-\t\t\t\t\t\t<div class="row">
-\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547698">
-\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
-\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
-\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547702" style="width: 45.2875px;">
-\t\t\t\t\t\t\t</div>
-\t\t\t\t\t\t</div>
-\t\t\t\t\t\t<div class="row">
-\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685575196">
-\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 26.6px;" value="sdsd">
-\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
-\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685575206" style="width: 45.2875px;">
-\t\t\t\t\t\t\t</div>
-\t\t\t\t\t\t</div>
-\t\t\t\t\t\t<div class="row">
-\t\t\t\t\t\t\t<div class="horizontal-group represent-node" data-node_represented="1848360241685547705">
-\t\t\t\t\t\t\t\t<input class="editable" placeholder="<no name>" required="" style="width: 10px;">
-\t\t\t\t\t\t\t\t<input class="fixed keyword" style="width: 40.225px;">
-\t\t\t\t\t\t\t\t<input class="fixed type represent-node" data-node_represented="1848360241685547711" style="width: 34.0125px;">
-\t\t\t\t\t\t\t</div>
-\t\t\t\t\t\t</div>
-\t\t\t\t\t</div>
-\t\t\t\t</div>
-\t\t\t</div>
-\t\t</div>
-\t</body>
-</html>`;
-
-function compareVNodes(rendered: VNode, expectedRendered: VNode) : void {
-    expect(rendered.sel).to.eql(expectedRendered.sel);
-    expect(rendered.data.props).to.eql(expectedRendered.data.props);
-    expect(rendered.data.dataset).to.eql(expectedRendered.data.dataset);
-    expect(rendered.children).deep.equal(expectedRendered.children);
-    expect(rendered.key).to.eql(expectedRendered.key);
-    expect(rendered.text).to.eql(expectedRendered.text);
-}
+import {compareVNodes} from "./testutils";
 
 const rootData1 = {
     "children": [
