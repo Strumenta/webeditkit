58dba62fa2a483ffb709d396414391c1918b67e4
Federico Tomassetti <federico@tomassetti.me>
2020-05-10 11:53:30 +0200

    fix mocha tester

--- tester.sh
+++ tester.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 FILES=test/*.spec.ts
+RESULT=0
 for f in $FILES
 do
   echo "Processing $f file..."
-  mocha -r ts-node/register $f
+  node ./node_modules/mocha/bin/mocha -r ts-node/register $f
+  PARTIAL_RESULT=$?
+  if [ $PARTIAL_RESULT -gt 0 ]; then
+  	RESULT=$PARTIAL_RESULT   
+  fi
 done
+exit $RESULT
