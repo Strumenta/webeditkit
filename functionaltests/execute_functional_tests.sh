f0f06b142b9b0f172ea7ad1a531f5d87426bbed8
Federico Tomassetti <federico@tomassetti.me>
2020-05-10 11:33:43 +0200

    more logs

--- functionaltests/execute_functional_tests.sh
+++ functionaltests/execute_functional_tests.sh
@@ -1,7 +1,11 @@
-./gradlew runMpsServerLauncher > server_log_out.txt 2>server_log_err.txt &
+./gradlew runMpsServerLauncher & #> server_log_out.txt 2>server_log_err.txt &
 MPS_SERVER_PID=$!
-sleep 2m # we need time to install the stuff
+sleep 4m # we need time to install the stuff
 sh run_functional_tests.sh
 RESULT=$?
 kill -9 $MPS_SERVER_PID
+echo "LOG"
+cat server_log_out.txt
+echo "LOG ERR"
+cat server_log_err.txt
 exit $RESULT
