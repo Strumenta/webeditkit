70d59de45cda9c5645ba4687d5ad884720eaef11
Federico Tomassetti <federico@tomassetti.me>
2020-05-10 11:28:37 +0200

    running functional tests on server

--- functionaltests/execute_functional_tests.sh
+++ functionaltests/execute_functional_tests.sh
@@ -1,5 +1,6 @@
 ./gradlew runMpsServerLauncher > server_log_out.txt 2>server_log_err.txt &
 MPS_SERVER_PID=$!
+sleep 2m # we need time to install the stuff
 sh run_functional_tests.sh
 RESULT=$?
 kill -9 $MPS_SERVER_PID
