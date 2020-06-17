7ec391016770bc2a67acbba58add05a52c0d0735
Federico Tomassetti <federico@tomassetti.me>
2020-06-17 08:13:14 +0200

    fix intentions tests

--- functionaltests/src/intentions.ts
+++ functionaltests/src/intentions.ts
@@ -4,9 +4,9 @@ import { XMLHttpRequest } from 'xmlhttprequest';
 import { MPSSERVER_PORT, reloadAll, tryToConnect } from './utils';
 import { createInstance, getWsCommunication } from '../../src/communication';
 import { GetNode, GetNodeAnswer } from '../../src/communication/messages';
-import * as delay from 'delay';
+import delay from 'delay';
 
-var W3CWebSocket = require('websocket').w3cwebsocket;
+const W3CWebSocket = require('websocket').w3cwebsocket;
 
 describe('Intentions API', () => {
   before(function (done) {
@@ -52,40 +52,80 @@ describe('Intentions API', () => {
     };
   });
 
-  // it('execute intention', (done) => {
-  //
-  //     // 1) Check initial name of project
-  //     // 2) Call intention
-  //     // 3) Verify final name of project
-  //
-  //     const ws  = new W3CWebSocket(`ws://localhost:${MPSSERVER_PORT}/socket`);
-  //     const nodeIdForProject1 = {
-  //         model: 'ExampleLanguage.sandbox',
-  //         id: {
-  //             regularId: '7467535778008417157'
-  //         }
-  //     };
-  //     const nodeIdForClient = {
-  //         model: 'ExampleLanguage.sandbox',
-  //         id: {
-  //             regularId: '7467535778008416706'
-  //         }
-  //     };
-  //     // tslint:disable-next-line:only-arrow-functions
-  //     ws.onopen = async function() {
-  //         const wsc = createInstance(`ws://localhost:${MPSSERVER_PORT}/socket`, 'ExampleLanguage.sandbox', 'foo',
-  //           ws);
-  //         const nodeData1 = await wsc.getNodeData(nodeIdForProject1);
-  //         expect(nodeData1.properties.id).eql('ACME-1');
-  //         const intentions = await wsc.getIntentions(nodeIdForClient);
-  //         expect(intentions.length).to.eql(1);
-  //         expect(intentions[0].index).to.eql(0);
-  //         expect(intentions[0].description).to.eql('Assign Standard ID to All Projects');
-  //         intentions[0].execute();
-  //         delay(400);
-  //         const nodeData2 = await wsc.getNodeData(nodeIdForProject1);
-  //         expect(nodeData2.properties.id).eql('ACBU');
-  //         done();
-  //     }
-  // });
+  it('execute intention from node', (done) => {
+
+    // 1) Check initial name of project
+    // 2) Call intention
+    // 3) Verify final name of project
+
+    const ws  = new W3CWebSocket(`ws://localhost:${MPSSERVER_PORT}/socket`);
+    const nodeIdForProject1 = {
+      model: 'ExampleLanguage.sandbox',
+      id: {
+        regularId: '7467535778008417157'
+      }
+    };
+    const nodeIdForClient = {
+      model: 'ExampleLanguage.sandbox',
+      id: {
+        regularId: '7467535778008416706'
+      }
+    };
+    // tslint:disable-next-line:only-arrow-functions
+    ws.onopen = async function() {
+      const wsc = createInstance(`ws://localhost:${MPSSERVER_PORT}/socket`, 'ExampleLanguage.sandbox', 'foo',
+        ws);
+      const nodeData1 = await wsc.getNodeData(nodeIdForProject1);
+      expect(nodeData1.properties.id).eql('ACME-1');
+      const intentions = await wsc.getIntentions(nodeIdForProject1);
+      expect(intentions.length).to.eql(2);
+      expect(intentions[1].index).to.eql(1);
+      expect(intentions[1].description).to.eql('Assign Standard ID to All Projects');
+      intentions[1].execute();
+      setTimeout(async ()=>{
+        const nodeData2 = await wsc.getNodeData(nodeIdForProject1);
+        expect(nodeData2.properties.id).eql('ACBU');
+        done();
+      }, 400);
+    }
+  });
+
+  it('execute intention from parent', (done) => {
+
+      // 1) Check initial name of project
+      // 2) Call intention
+      // 3) Verify final name of project
+
+      const ws  = new W3CWebSocket(`ws://localhost:${MPSSERVER_PORT}/socket`);
+      const nodeIdForProject1 = {
+          model: 'ExampleLanguage.sandbox',
+          id: {
+              regularId: '7467535778008417157'
+          }
+      };
+      const nodeIdForClient = {
+          model: 'ExampleLanguage.sandbox',
+          id: {
+              regularId: '7467535778008416706'
+          }
+      };
+      // tslint:disable-next-line:only-arrow-functions
+      ws.onopen = async function() {
+          const wsc = createInstance(`ws://localhost:${MPSSERVER_PORT}/socket`, 'ExampleLanguage.sandbox', 'foo',
+            ws);
+          const nodeData1 = await wsc.getNodeData(nodeIdForProject1);
+          expect(nodeData1.properties.id).eql('ACME-1');
+          const intentions = await wsc.getIntentions(nodeIdForClient);
+          expect(intentions.length).to.eql(1);
+          expect(intentions[0].index).to.eql(0);
+          expect(intentions[0].description).to.eql('Assign Standard ID to All Projects');
+          intentions[0].execute();
+          setTimeout(async ()=>{
+            const nodeData2 = await wsc.getNodeData(nodeIdForProject1);
+            expect(nodeData2.properties.id).eql('ACBU');
+            done();
+          }, 400);
+
+      }
+  });
 });
