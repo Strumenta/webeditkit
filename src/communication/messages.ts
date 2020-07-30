e74c8769128189de8c8e3e339f4c6f014e71e344
Federico Tomassetti <federico@tomassetti.me>
2020-07-30 12:37:05 +0200

    add concept messages

--- src/communication/messages.ts
+++ src/communication/messages.ts
@@ -285,3 +285,61 @@ export interface RegisterForChanges extends Message {
   type: 'registerForChanges';
   modelName: string;
 }
+
+//
+// Language and concept
+//
+
+export interface Link {
+  name: string;
+  optional: boolean;
+  type: string;
+}
+
+export interface Containment extends Link {
+  multiple: boolean;
+}
+
+type Reference = Link;
+
+export interface Property {
+  name: string;
+  type: string;
+}
+
+export interface Concept {
+  qualifiedName: string;
+  alias: string;
+  isInterface: boolean;
+  isAbstract: boolean;
+  rootable: boolean;
+  superConcept?: string;
+  interfaceConcepts: string[];
+  declaredContainments: Containment[];
+  inheritedContainments: Containment[];
+  declaredReferences: Reference[];
+  inheritedReferences: Reference[];
+  declaredProperties: Property[];
+  inheritedProperties: Property[];
+}
+
+export interface EnumLiteral {
+  name: string;
+  label: string;
+}
+
+export interface Enum {
+  name: string;
+  defaultLiteral?: string;
+  literals: EnumLiteral[];
+}
+
+export interface LanguageInfo {
+  qualifiedName: string;
+  sourceModuleName: string;
+}
+
+export interface LanguageInfoDetailed extends LanguageInfo {
+  concepts: Concept[];
+  enums: Enum[];
+}
